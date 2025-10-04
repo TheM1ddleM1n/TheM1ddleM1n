acd0f636f3ed348f8803192502f8b8fab7571fe7
TheM1ddleM1n <192749026+TheM1ddleM1n@users.noreply.github.com>
2025-10-04 19:28:35 +0100

    Delete scripts/countdown.js
    nah

--- scripts/countdown.js
+++
@@ -1,8 +0,0 @@
-let count = 20;
-let interval = setInterval(() => {
-  process.stdout.write("\rCountdown: " + count);
-  if (count-- <= 0) {
-    clearInterval(interval);
-    console.log("\nTime's up!");
-  }
-}, 1000);
